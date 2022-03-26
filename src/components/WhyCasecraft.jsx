c2d5c84fdd02667182ac02ada0a3e9908765ba6e
babblebey <bode@bode.me>
2022-03-26 22:07:15 +0100

    updated WhyCasecraft component

--- src/components/WhyCasecraft.jsx
+++ src/components/WhyCasecraft.jsx
@@ -2,7 +2,7 @@ import airpod from '../img/airpod.svg';
 
 const WhyCasecraft = () => {
     return ( 
-        <div className="flex flex-col md:flex-row justify-center items-center w-full p-10 lg:w-9/12 m-auto">
+        <div className="flex flex-col md:flex-row justify items-center w-full p-10 lg:w-9/12 m-auto">
             <div className="w-full md:w-6/12 my-4 text-center sm:text-left">
                 <h2 className="italic font-bold text-primary-300 my-7 text-lg lg:text-[30px]">
                     Why CaseCraft?
@@ -16,7 +16,7 @@ const WhyCasecraft = () => {
             </div>
             <div className="w-full md:w-fit">
                 <img src={airpod} alt="Airpod" className="w-full" />
-                <p className="italic text-sm text-center relative -top-10">Most sold of the month</p>
+                <p className="italic text-sm text-center relative -top-8 md:-top-10">Most sold of the month</p>
             </div>
         </div>
     );
